b92e8755c5f6f8ead30aa694219da4fba80e1be3
PropeReferio <lemuel.b.stevens@gmail.com>
2020-07-31 16:10:16 -0500

    cross off complete

--- src/App.js
+++ src/App.js
@@ -3,20 +3,50 @@ import './App.css';
 import v4 from 'uuid';
 
 function App() {
-  const [ tasks, setTasks ] = useState({list: ['Test task', 'other task', 'blah task']})
+  const [ tasks, setTasks ] = useState([{task: 'Learn Kubernetes',
+                                         done: false},
+                                         {task: 'Make Standing Desk',
+                                         done: false},
+                                         ])
   const [ formVal, setFormVal ] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTasks([...tasks, {task: formVal, done: false}]);
+    // Todo: clear form after submit
+  };
+
+  const handleChange = e => {
+    setFormVal(e.target.value);
+  }
+
+  const handleClick = index => {
+    // This is probably a good place to use useReducer
+    let newTasks = [...tasks]
+    newTasks[index] = { ...tasks[index], done: true }
+    setTasks(newTasks);
+  }
 
   return (
     <>
       <h1>To Do List</h1>
       <br />
+      <p>Click a completed task to scratch it off</p>
       <ol>
         {
-          tasks.list.map(item => (
-            <li key={v4()}>{item}</li>
+          tasks.map((item, index) => (
+            <li key={index} 
+                className={item.done ? 'done' : ''} 
+                onClick={() => {handleClick(index)}}>
+                  {item.task}
+            </li>
           ))
         }
       </ol>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='What to do?' onChange={handleChange}></input>
+        <input type="submit" value='Add Task' />
+      </form>
     </>
   );
 }
